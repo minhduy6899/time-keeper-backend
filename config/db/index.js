31790582c52ed4bfae5df014af634ec8d6bd0267
Minh Duy <minhduy140799@gmail.com>
2022-10-29 21:10:09 +0700

    commit

--- config/db/index.js
+++ config/db/index.js
@@ -1,39 +1,37 @@
 const mongoose = require('mongoose');
 
-// async function connect() {
-//   try {
-//     await mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@time-keeper-shop.e82m1hm.mongodb.net/?retryWrites=true&w=majority`, {
-//       useCreateIndex: true,
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//       useFindAndModify: false
-//     });
-//     console.log('connect successfully!!!!');
-//   } catch (error) {
-//     console.log('connect failed!!!!');
-
-//   }
-// }
-
-
-
-const connect = async () => {
+async function connect() {
   try {
-    await mongoose.connect(
-      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.htal2au.mongodb.net/?retryWrites=true&w=majority`,
-      {
-        useCreateIndex: true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-      }
-    )
-
-    console.log('MongoDB connected')
+    await mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@time-keeper-shop.e82m1hm.mongodb.net/?retryWrites=true&w=majority`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('connect successfully!!!!');
   } catch (error) {
-    console.log(error.message)
-    process.exit(1)
+    console.log('connect failed!!!!');
+
   }
 }
+
+
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(
+//       `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.htal2au.mongodb.net/?retryWrites=true&w=majority`,
+//       {
+//         useCreateIndex: true,
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useFindAndModify: false
+//       }
+//     )
+
+//     console.log('MongoDB connected')
+//   } catch (error) {
+//     console.log(error.message)
+//     process.exit(1)
+//   }
+// }
 
 module.exports = { connect };
