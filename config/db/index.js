e4af667ad3f7f66c3f88895a53e4b82cf159cf7d
Minh Duy <minhduy140799@gmail.com>
2022-10-29 21:06:06 +0700

    commit

--- config/db/index.js
+++ config/db/index.js
@@ -1,15 +1,38 @@
 const mongoose = require('mongoose');
 
-async function connect() {
-  try {
-    await mongoose.connect(`mongodb+srv://minhduy:${process.env.DB_PASSWORD}@time-keeper-shop.e82m1hm.mongodb.net/?retryWrites=true&w=majority`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('connect successfully!!!!');
-  } catch (error) {
-    console.log('connect failed!!!!');
+// async function connect() {
+//   try {
+//     await mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@time-keeper-shop.e82m1hm.mongodb.net/?retryWrites=true&w=majority`, {
+//       useCreateIndex: true,
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useFindAndModify: false
+//     });
+//     console.log('connect successfully!!!!');
+//   } catch (error) {
+//     console.log('connect failed!!!!');
 
+//   }
+// }
+
+
+
+const connect = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.htal2au.mongodb.net/?retryWrites=true&w=majority`,
+      {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+      }
+    )
+
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.log(error.message)
+    process.exit(1)
   }
 }
 
